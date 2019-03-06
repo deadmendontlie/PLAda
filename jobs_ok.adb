c5d93d92129d21d19f572011993f70985f8377b2
Deadmendontlie <33693665+deadmendontlie@users.noreply.github.com>
2019-03-06 14:45:43 -0500

    Add files via upload

--- jobs_ok.adb
+++ jobs_ok.adb
@@ -1,25 +1,38 @@
-with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics, Ada.Strings.Unbounded;
-use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics, Ada.String.Unbounded;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics;
+use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics;
 
 procedure jobs_ok is 
-	type Employees is array (Positive range <>) of Block_Of_Characters;
-	Blocks: Employees (1 ..26);
-	type TimeSheet is array (Positive range <>) of Block_Of_Characters;
-        Blocks: TimeSheet (1 ..4);
+	type NameAndSkills is record
+		Person: Character;
+		Phone: Integer;
+		Computer: Integer;
+		Netowrk: Integer;
+	end record;
+	type Employees is array (1..26) of NamesAndSkills;
+	Employee: Employees;
+	type TimeSheet is array (1..4,1..4) of Character;
+        ShiftsList: TimeSheet;
 	NumberOfEmployees: Integer;
-	N: Integer;
-	M: Integer;
-	Add in extra methods here
-
+	WhiteSpace: Character;
+	--Add in extra methods here
+	
 
 
 begin
-	N := 1;
-	for N in 1..20 loop
-		Get_Line(Item => TimeSheeet(N));
+	for Shift in 1..4 loop
+		for Job in 1..3 loop
+			Get(ShiftsList(Shift, Job));
+			Get(WhiteSpace);
+		end loop;
+			Get(ShiftsList(Shift, 4));
+		Skip_Line;
 	end loop;
 	Get(NumberOfEmployees);
-	M := 1;
-	for m in 1..NumberOfEmployees loop
-		Get_Line(Item => Employees(M));
+	Skip_Line;
+	for Line in 1..NumberOfEmployees loop
+		Get(TimeSheet(Line).Person);
+		Get(TimeSheet(Line).Phone);
+		Get(TimeSheet(Line).Computer);
+		Get(TimeSheet(Line).Network);
+		Skip_Line;
 	end loop;
